a128f2f8767ef49a4715765cc7774fe0edbac7eb
Masahiro Fujimura <mur4m4s4.331@gmail.com>
2020-01-25 16:10:37 +0900

    Ignore dev version (#27)
    * Ignore dev version
    
    * Review commit

--- photon/photon.go
+++ photon/photon.go
@@ -60,6 +60,9 @@ func (c Config) Update() error {
 		return xerrors.Errorf("failed to get Photon versions: %w", err)
 	}
 	for _, version := range versions {
+		if strings.ToLower(version) == "dev" {
+			continue
+		}
 		res, err := utils.FetchURL(c.URL+fmt.Sprintf(advisoryFormat, version), "", c.Retry)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch Photon advisory: %w", err)
