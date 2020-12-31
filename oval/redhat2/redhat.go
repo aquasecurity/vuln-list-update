5ba879954af3c7d4bb199fdd720cf8173272b130
knqyf263 <knqyf263@gmail.com>
2020-12-31 07:07:22 +0200

    refactor

--- oval/redhat2/redhat.go
+++ oval/redhat2/redhat.go
@@ -105,17 +105,17 @@ func (c Config) update(ovalFile string) error {
 	dirPath := filepath.Join(c.VulnListDir, ovalDir, redhatDir, release, platform)
 
 	// write tests/tests.json file
-	if err := c.writeJson(filepath.Join(dirPath, testsDir), "tests.json", ovalroot.Tests); err != nil {
+	if err := c.writeJSON(filepath.Join(dirPath, testsDir), "tests.json", ovalroot.Tests); err != nil {
 		return xerrors.Errorf("failed to write tests: %w", err)
 	}
 
 	// write objects/objects.json file
-	if err := c.writeJson(filepath.Join(dirPath, objectsDir), "objects.json", ovalroot.Objects); err != nil {
+	if err := c.writeJSON(filepath.Join(dirPath, objectsDir), "objects.json", ovalroot.Objects); err != nil {
 		return xerrors.Errorf("failed to write objects: %w", err)
 	}
 
 	// write states/states.json file
-	if err := c.writeJson(filepath.Join(dirPath, statesDir), "states.json", ovalroot.States); err != nil {
+	if err := c.writeJSON(filepath.Join(dirPath, statesDir), "states.json", ovalroot.States); err != nil {
 		return xerrors.Errorf("failed to write states: %w", err)
 	}
 
@@ -197,13 +197,13 @@ func (c Config) saveAdvisoryPerYear(dirName string, id string, def Definition) e
 	}
 
 	yearDir := filepath.Join(dirName, year)
-	if err := c.writeJson(yearDir, fmt.Sprintf(fileFmt, id), def); err != nil {
+	if err := c.writeJSON(yearDir, fmt.Sprintf(fileFmt, id), def); err != nil {
 		return xerrors.Errorf("unable to write a JSON file: %w", err)
 	}
 	return nil
 }
 
-func (c Config) writeJson(dirName, fileName string, data interface{}) error {
+func (c Config) writeJSON(dirName, fileName string, data interface{}) error {
 	if err := c.AppFs.MkdirAll(dirName, os.ModePerm); err != nil {
 		return xerrors.Errorf("failed to create a year dir: %w", err)
 	}
