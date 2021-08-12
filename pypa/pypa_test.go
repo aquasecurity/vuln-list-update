a33d1f9d9e487c51e2d8efc97ddc1afe65bbc457
Andrey Levchenko <levchenko.andrey@gmail.com>
2021-08-12 21:40:23 +0600

    feat(python): add pypa support
    add test

--- pypa/pypa_test.go
+++ pypa/pypa_test.go
@@ -1,15 +1,78 @@
 package pypa
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Update(t *testing.T) {
-	pypa := NewPypa()
-	os.RemoveAll(pypa.opts.dir)
-	err := pypa.Update()
-	if err != nil {
-		t.Errorf("error: %v\n", err)
+	tests := []struct {
+		name          string
+		inputArchive  string
+		expectedFiles []string
+		wantErr       string
+	}{
+		{
+			name:          "happy path",
+			inputArchive:  "testdata/pypa.zip",
+			expectedFiles: []string{"PYSEC-2005-1.json", "PYSEC-2006-1.json", "PYSEC-2006-2.json"},
+		},
+		{
+			name:    "sad path, unable to download archive",
+			wantErr: "connection refused",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				b, err := os.ReadFile(tt.inputArchive)
+				require.NoError(t, err)
+
+				w.Write(b)
+			}))
+
+			defer ts.Close()
+
+			// Intentionally close to induce network errors
+			if tt.inputArchive == "" {
+				ts.Close()
+			}
+
+			dir := t.TempDir()
+			c := NewPypa(WithURL(ts.URL+"/"+tt.inputArchive), WithDir(filepath.Join(dir)), WithRetry(0))
+			err := c.Update()
+			if tt.wantErr != "" {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tt.wantErr)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			entries, err := os.ReadDir("testdata/golden")
+			require.NoError(t, err)
+
+			for _, e := range entries {
+				if e.IsDir() {
+					continue
+				}
+
+				filePath := e.Name()
+				gotJSON, err := os.ReadFile(filepath.Join(dir, filePath))
+				require.NoError(t, err)
+
+				wantJSON, err := os.ReadFile(filepath.Join("testdata", "golden", filepath.Base(filePath)))
+				require.NoError(t, err)
+
+				assert.JSONEq(t, string(wantJSON), string(gotJSON))
+			}
+		})
 	}
 }
