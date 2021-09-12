1dc8ef572b8c76667352357886ef056caf592899
knqyf263 <knqyf263@gmail.com>
2021-09-12 20:26:53 +0300

    refactor(main): fix error messages

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func run() error {
 	switch *target {
 	case "nvd":
 		if err := nvd.Update(now.Year()); err != nil {
-			return xerrors.Errorf("error in NVD update: %w", err)
+			return xerrors.Errorf("NVD update error: %w", err)
 		}
 		commitMsg = "NVD"
 	case "redhat":
@@ -100,41 +100,41 @@ func run() error {
 			return xerrors.New("years must be specified")
 		}
 		if err := securitydataapi.Update(yearList); err != nil {
-			return err
+			return xerrors.Errorf("Red Hat Security Data API update error: %w", err)
 		}
 		commitMsg = "RedHat " + *years
 	case "redhat-oval":
 		rc := redhatoval.NewConfig()
 		if err := rc.Update(); err != nil {
-			return xerrors.Errorf("error in Red Hat OVAL v2 update: %w", err)
+			return xerrors.Errorf("Red Hat OVALv2 update error: %w", err)
 		}
 		commitMsg = "Red Hat OVAL v2"
 	case "debian":
 		dc := tracker.NewClient()
 		if err := dc.Update(); err != nil {
-			return xerrors.Errorf("error in Debian update: %w", err)
+			return xerrors.Errorf("Debian update error: %w", err)
 		}
 		commitMsg = "Debian Security Bug Tracker"
 	case "debian-oval":
 		if err := debianoval.Update(); err != nil {
-			return xerrors.Errorf("error in Debian OVAL update: %w", err)
+			return xerrors.Errorf("Debian OVAL update error: %w", err)
 		}
 		commitMsg = "Debian OVAL"
 	case "ubuntu":
 		if err := ubuntu.Update(); err != nil {
-			return xerrors.Errorf("error in Debian update: %w", err)
+			return xerrors.Errorf("Ubuntu update error: %w", err)
 		}
 		commitMsg = "Ubuntu CVE Tracker"
 	case "alpine":
 		au := alpine.NewUpdater()
 		if err := au.Update(); err != nil {
-			return xerrors.Errorf("error in Alpine update: %w", err)
+			return xerrors.Errorf("Alpine update error: %w", err)
 		}
 		commitMsg = "Alpine Issue Tracker"
 	case "alpine-unfixed":
 		au := alpineunfixed.NewUpdater()
 		if err := au.Update(); err != nil {
-			return xerrors.Errorf("error in Alpine update: %w", err)
+			return xerrors.Errorf("Alpine Secfixes Tracker update error: %w", err)
 		}
 		commitMsg = "Alpine Secfixes Tracker"
 	case "amazon":
@@ -143,25 +143,25 @@ func run() error {
 			VulnListDir:        utils.VulnListDir(),
 		}
 		if err := ac.Update(); err != nil {
-			return xerrors.Errorf("error in Amazon update: %w", err)
+			return xerrors.Errorf("Amazon Linux update error: %w", err)
 		}
 		commitMsg = "Amazon Linux Security Center"
 	case "oracle-oval":
 		oc := oracleoval.NewConfig()
 		if err := oc.Update(); err != nil {
-			return xerrors.Errorf("error in Oracle Linux OVAL update: %w", err)
+			return xerrors.Errorf("Oracle OVAL update error: %w", err)
 		}
 		commitMsg = "Oracle Linux OVAL"
 	case "suse-cvrf":
 		sc := susecvrf.NewConfig()
 		if err := sc.Update(); err != nil {
-			return xerrors.Errorf("error in SUSE CVRF update: %w", err)
+			return xerrors.Errorf("SUSE CVRF update error: %w", err)
 		}
 		commitMsg = "SUSE CVRF"
 	case "photon":
 		pc := photon.NewConfig()
 		if err := pc.Update(); err != nil {
-			return xerrors.Errorf("error in Photon update: %w", err)
+			return xerrors.Errorf("Photon update error: %w", err)
 		}
 		commitMsg = "Photon Security Advisories"
 	case "ghsa":
@@ -172,31 +172,31 @@ func run() error {
 
 		gc := ghsa.NewConfig(githubql.NewClient(httpClient))
 		if err := gc.Update(); err != nil {
-			return xerrors.Errorf("error in GitHub Security Advisory update: %w", err)
+			return xerrors.Errorf("GitHub Security Advisory update error: %w", err)
 		}
 		commitMsg = "GitHub Security Advisory"
 	case "glad":
 		gu := glad.NewUpdater()
 		if err := gu.Update(); err != nil {
-			return xerrors.Errorf("error in GitLab Advisory Database update: %w", err)
+			return xerrors.Errorf("GitLab Advisory Database update error: %w", err)
 		}
 		commitMsg = "GitLab Advisory Database"
 	case "cwe":
 		c := cwe.NewCWEConfig()
 		if err := c.Update(); err != nil {
-			return xerrors.Errorf("error in CWE update: %w", err)
+			return xerrors.Errorf("CWE update error: %w", err)
 		}
 		commitMsg = "CWE Advisories"
 	case "arch-linux":
 		al := arch_linux.NewArchLinux()
 		if err := al.Update(); err != nil {
-			return xerrors.Errorf("error in CWE update: %w", err)
+			return xerrors.Errorf("Arch Linux update error: %w", err)
 		}
 		commitMsg = "Arch Linux Security Tracker"
 	case "alma":
 		ac := alma.NewConfig()
 		if err := ac.Update(); err != nil {
-			return xerrors.Errorf("error in AlmaLinux update: %w", err)
+			return xerrors.Errorf("AlmaLinux update error: %w", err)
 		}
 		commitMsg = "AlmaLinux Security Advisory"
 	default:
