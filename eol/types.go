d343aebf6bd730de1e93c66a8a47a4ae9a451c1e
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-04-20 17:37:24 +0600

    feat(eol): added debian EOL dates

--- eol/types.go
+++ eol/types.go
@@ -3,6 +3,7 @@ package eol
 import (
 	"github.com/aquasecurity/vuln-list-update/eol/alma"
 	"github.com/aquasecurity/vuln-list-update/eol/alpine"
+	"github.com/aquasecurity/vuln-list-update/eol/debian"
 	"github.com/aquasecurity/vuln-list-update/eol/rocky"
 )
 
@@ -10,4 +11,5 @@ var all = []EolSrc{
 	alma.NewConfig(),
 	alpine.NewConfig(),
 	rocky.NewConfig(),
+	debian.NewConfig(),
 }
