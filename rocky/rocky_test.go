b19cb98d6ac9bf5f595c70f10f58c30ef9b589c2
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2022-05-26 10:31:24 +0600

    fix(rocky): use minor part in version number (#154)
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

--- rocky/rocky_test.go
+++ rocky/rocky_test.go
@@ -1,7 +1,6 @@
 package rocky_test
 
 import (
-	"errors"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -11,57 +10,72 @@ import (
 	"github.com/aquasecurity/vuln-list-update/rocky"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/xerrors"
 )
 
 func Test_Update(t *testing.T) {
 	tests := []struct {
-		name          string
-		rootDir       string
-		repository    []string
-		expectedError error
+		name             string
+		releasesFilePath string
+		rootDir          string
+		repository       []string
+		wantErr          string
 	}{
 		{
-			name:          "happy path",
-			rootDir:       "testdata/fixtures/happy",
-			repository:    []string{"BaseOS"},
-			expectedError: nil,
+			name:             "happy path",
+			releasesFilePath: "testdata/fixtures/releases/happy.html",
+			rootDir:          "testdata/fixtures/happy",
+			repository:       []string{"BaseOS"},
 		},
 		{
-			name:          "bad repomd response",
-			rootDir:       "testdata/fixtures/repomd_invalid",
-			repository:    []string{"BaseOS"},
-			expectedError: xerrors.Errorf("failed to update security advisories of Rocky Linux 8 BaseOS x86_64: %w", errors.New("failed to fetch updateInfo path from repomd.xml")),
+			name:             "bad repomd response",
+			releasesFilePath: "testdata/fixtures/releases/happy.html",
+			rootDir:          "testdata/fixtures/repomd_invalid",
+			repository:       []string{"BaseOS"},
+			wantErr:          "failed to fetch updateInfo path from repomd.xml",
 		},
 		{
-			name:          "bad updateInfo response",
-			rootDir:       "testdata/fixtures/updateinfo_invalid",
-			repository:    []string{"BaseOS"},
-			expectedError: xerrors.Errorf("failed to update security advisories of Rocky Linux 8 BaseOS x86_64: %w", errors.New("failed to fetch updateInfo")),
+			name:             "bad updateInfo response",
+			releasesFilePath: "testdata/fixtures/releases/happy.html",
+			rootDir:          "testdata/fixtures/updateinfo_invalid",
+			repository:       []string{"BaseOS"},
+			wantErr:          "failed to fetch updateInfo",
 		},
 		{
-			name:          "no updateInfo field(BaseOS)",
-			rootDir:       "testdata/fixtures/no_updateinfo_field",
-			repository:    []string{"BaseOS"},
-			expectedError: xerrors.Errorf("failed to update security advisories of Rocky Linux 8 BaseOS x86_64: %w", xerrors.Errorf("failed to fetch updateInfo path from repomd.xml: %w", rocky.ErrorNoUpdateInfoField)),
+			name:             "no updateInfo field(BaseOS)",
+			releasesFilePath: "testdata/fixtures/releases/happy.html",
+			rootDir:          "testdata/fixtures/no_updateinfo_field",
+			repository:       []string{"BaseOS"},
+			wantErr:          rocky.ErrorNoUpdateInfoField.Error(),
 		},
 		{
-			name:          "no updateInfo field(extras)",
-			rootDir:       "testdata/fixtures/no_updateinfo_field",
-			repository:    []string{"extras"},
-			expectedError: nil,
+			name:             "no updateInfo field(extras)",
+			releasesFilePath: "testdata/fixtures/releases/happy.html",
+			rootDir:          "testdata/fixtures/no_updateinfo_field",
+			repository:       []string{"extras"},
+		},
+		{
+			name:             "empty list of releases",
+			releasesFilePath: "testdata/fixtures/releases/empty.html",
+			repository:       []string{"BaseOS"},
+			wantErr:          "list is empty",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tsUpdateInfoURL := httptest.NewServer(http.StripPrefix("/pub/rocky/8/BaseOS/x86_64/os/repodata/", http.FileServer(http.Dir(tt.rootDir))))
+			mux := http.NewServeMux()
+			mux.Handle("/pub/rocky/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.ServeFile(w, r, tt.releasesFilePath)
+			}))
+			mux.Handle("/pub/rocky/8.5/extras/x86_64/os/repodata/", http.StripPrefix("/pub/rocky/8.5/extras/x86_64/os/repodata/", http.FileServer(http.Dir(tt.rootDir))))
+			mux.Handle("/pub/rocky/8.5/BaseOS/x86_64/os/repodata/", http.StripPrefix("/pub/rocky/8.5/BaseOS/x86_64/os/repodata/", http.FileServer(http.Dir(tt.rootDir))))
+			tsUpdateInfoURL := httptest.NewServer(mux)
 			defer tsUpdateInfoURL.Close()
 
 			dir := t.TempDir()
-			rc := rocky.NewConfig(rocky.With(tsUpdateInfoURL.URL+"/pub/rocky/%s/%s/%s/os/", dir, 0, []string{"8"}, tt.repository, []string{"x86_64"}))
-			if err := rc.Update(); tt.expectedError != nil {
+			rc := rocky.NewConfig(rocky.With(tsUpdateInfoURL.URL+"/pub/rocky", "%s/%s/%s/%s/os/", dir, 0, tt.repository, []string{"x86_64"}))
+			if err := rc.Update(); tt.wantErr != "" {
 				require.Error(t, err)
-				assert.Contains(t, err.Error(), tt.expectedError.Error())
+				assert.Contains(t, err.Error(), tt.wantErr)
 				return
 			}
 
