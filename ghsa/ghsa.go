20aa347c5ccd283594ed3cb9fc8f9104ff1d3c3a
AMF <work@afdesk.com>
2022-08-25 11:51:45 +0600

    feat: add Rust ecosystem from GitHub Advisory Database

--- ghsa/ghsa.go
+++ ghsa/ghsa.go
@@ -29,8 +29,9 @@ var (
 	Pip        SecurityAdvisoryEcosystem = "PIP"
 	Rubygems   SecurityAdvisoryEcosystem = "RUBYGEMS"
 	Go         SecurityAdvisoryEcosystem = "GO"
+	Rust       SecurityAdvisoryEcosystem = "RUST"
 	Erlang     SecurityAdvisoryEcosystem = "ERLANG"
-	Ecosystems                           = []SecurityAdvisoryEcosystem{Composer, Maven, Npm, Nuget, Pip, Rubygems, Go, Erlang}
+	Ecosystems                           = []SecurityAdvisoryEcosystem{Composer, Maven, Npm, Nuget, Pip, Rubygems, Go, Erlang, Rust}
 
 	wait = func(i int) time.Duration {
 		sleep := math.Pow(float64(i), 2) + float64(utils.RandInt()%10)
