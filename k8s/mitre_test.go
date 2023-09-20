e3980ae18f528ac12d94545ef2d0ffe51f451d89
chenk <hen.keinan@gmail.com>
2023-09-20 13:59:21 +0300

    feat: k8s as osv db support
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- k8s/mitre_test.go
+++ k8s/mitre_test.go
@@ -14,14 +14,12 @@ func TestSanitizedVersion(t *testing.T) {
 	}{
 		{Name: "validate n/a version ", Version: &MitreVersion{Version: "n/a"}, Want: &MitreVersion{Version: "n/a"}},
 		{Name: "validate unspecified version ", Version: &MitreVersion{Version: "unspecified"}, Want: &MitreVersion{Version: "unspecified"}},
-		{Name: "validate less equal sign in version", Version: &MitreVersion{Version: "<= 1.3.4"}, Want: &MitreVersion{Version: "<= 1.3.4", LessThanOrEqual: "1.3.4"}},
 		{Name: "validate less equal sign and version", Version: &MitreVersion{LessThanOrEqual: "<=", Version: "1.3.4"}, Want: &MitreVersion{Version: "1.3.4", LessThanOrEqual: "1.3.4"}},
 		{Name: "validate less sign in version", Version: &MitreVersion{Version: "< 1.3.4"}, Want: &MitreVersion{Version: "< 1.3.4", LessThan: "1.3.4"}},
 		{Name: "validate prior to then sign in version", Version: &MitreVersion{Version: "prior to 1.3.4"}, Want: &MitreVersion{Version: "prior to 1.3.4", LessThan: "1.3.4"}},
 		{Name: "validate prior to with minor in version", Version: &MitreVersion{Version: "prior to 1.3"}, Want: &MitreVersion{Version: "1.3.0", LessThan: "1.3.0"}},
 		{Name: "validate less  with astrix", Version: &MitreVersion{LessThan: "1.3*"}, Want: &MitreVersion{Version: "1.3"}},
 		{Name: "validate less  with x", Version: &MitreVersion{Version: "1.3.x"}, Want: &MitreVersion{Version: "1.3"}},
-		{Name: "validate less equal sign in version", Version: &MitreVersion{LessThanOrEqual: "<= 1.3.4"}, Want: &MitreVersion{LessThanOrEqual: "1.3.4"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
