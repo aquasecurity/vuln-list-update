b419cff597f094bdb84f865a78d0d6151b759407
knqyf263 <knqyf263@gmail.com>
2023-12-17 18:40:59 +0400

    chore: lint fixes
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- nvd/nvd_test.go
+++ nvd/nvd_test.go
@@ -1,7 +1,6 @@
 package nvd_test
 
 import (
-	"github.com/aquasecurity/vuln-list-update/nvd"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -13,6 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/aquasecurity/vuln-list-update/nvd"
 	"github.com/aquasecurity/vuln-list-update/utils"
 )
 
