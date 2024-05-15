7074604717e7a6a93d6a4a689d4098cc9b7268bd
DmitriyLewen <dmitriy.lewen@smartforce.io>
2024-05-15 17:15:16 +0600

    test: add tests for `AdvisoryID`

--- mariner/mariner_test.go
+++ mariner/mariner_test.go
@@ -66,3 +66,69 @@ func TestUpdate(t *testing.T) {
 		})
 	}
 }
+
+func TestAdvisoryID(t *testing.T) {
+	tests := []struct {
+		name string
+		def  mariner.Definition
+		want string
+	}{
+		{
+			name: "advisory_id without version",
+			def: mariner.Definition{
+				Metadata: mariner.Metadata{
+					AdvisoryID: "1111",
+				},
+			},
+			want: "1111",
+		},
+		{
+			name: "advisory_id with version",
+			def: mariner.Definition{
+				Metadata: mariner.Metadata{
+					AdvisoryID: "1111-2",
+				},
+			},
+			want: "1111-2",
+		},
+		{
+			name: "build advisoryID converting long version to 1",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "2000000001",
+			},
+			want: "27423-1",
+		},
+		{
+			name: "build advisoryID converting long version to 0",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "2000000000",
+			},
+			want: "27423",
+		},
+		{
+			name: "build advisoryID with short 1 version",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "1",
+			},
+			want: "27423-1",
+		},
+		{
+			name: "build advisoryID with short 0 version",
+			def: mariner.Definition{
+				ID:      "oval:com.microsoft.cbl-mariner:def:27423",
+				Version: "0",
+			},
+			want: "27423",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := mariner.AdvisoryID(tt.def)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
