d7ff9fade9ebafdd04ae6659a5b1fb573190de85
David du Colombier <0intro@gmail.com>
2026-06-26 16:53:11 +0200

    feat(bottlerocket): add Bottlerocket security advisory fetcher (#406)
    Fetch and convert Bottlerocket security advisories from
    advisories.bottlerocket.aws/updateinfo.xml.gz into per-advisory
    JSON files for consumption by trivy-db.

---
+++ bottlerocket/types.go
@@ -0,0 +1,39 @@
+package bottlerocket
+
+// UpdateInfo represents the top-level structure of the updateinfo.xml file
+type UpdateInfo struct {
+	Updates []Update `xml:"update"`
+}
+
+// Update represents a single security advisory entry
+type Update struct {
+	ID          string      `xml:"id" json:"id,omitempty"`
+	Title       string      `xml:"title" json:"title,omitempty"`
+	Issued      Date        `xml:"issued" json:"issued,omitempty"`
+	Updated     Date        `xml:"updated" json:"updated,omitempty"`
+	Severity    string      `xml:"severity" json:"severity,omitempty"`
+	Description string      `xml:"description" json:"description,omitempty"`
+	Packages    []Package   `xml:"pkglist>collection>package" json:"packages,omitempty"`
+	References  []Reference `xml:"references>reference" json:"references,omitempty"`
+}
+
+// Date holds an advisory timestamp
+type Date struct {
+	Date string `xml:"date,attr" json:"date,omitempty"`
+}
+
+// Reference holds a vulnerability reference (CVE, GHSA, BRSA, etc.)
+type Reference struct {
+	Href string `xml:"href,attr" json:"href,omitempty"`
+	ID   string `xml:"id,attr" json:"id,omitempty"`
+	Type string `xml:"type,attr" json:"type,omitempty"`
+}
+
+// Package holds information about an affected/fixed package
+type Package struct {
+	Name    string `xml:"name,attr" json:"name,omitempty"`
+	Epoch   string `xml:"epoch,attr" json:"epoch,omitempty"`
+	Version string `xml:"version,attr" json:"version,omitempty"`
+	Release string `xml:"release,attr" json:"release,omitempty"`
+	Arch    string `xml:"arch,attr" json:"arch,omitempty"`
+}
